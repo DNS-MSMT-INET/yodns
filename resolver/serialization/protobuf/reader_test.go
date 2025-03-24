e59d9e9563e78b8029d79ec8c91d8b4016a65463
LAP-21-0158\fsteurer <fsteurer@mpi-inf.mpg.de>
2025-03-24 11:24:49 +0100

    test: long uuid format reader test

--- resolver/serialization/protobuf/reader_test.go
+++ resolver/serialization/protobuf/reader_test.go
@@ -1,18 +1,18 @@
 package protobuf
 
 import (
-	"fmt"
 	"github.com/DNS-MSMT-INET/yodns/resolver"
 	"github.com/DNS-MSMT-INET/yodns/resolver/serialization"
 	"testing"
 )
 
-func TestReader_ReadTo(t *testing.T) {
+// TestReader_CanReadLegacyFileFormat tests if the reader can read the legacy file format
+// that uses 16 byte UUIDs instead of snowflake IDs.
+func TestReader_CanReadLegacyFileFormat(t *testing.T) {
 	zip := serialization.ZipZSTD
-	r, _ := NewFileReader("C:\\Users\\fsteurer.LAP-21-0158\\Documents\\dnsmonitor\\experiments\\data\\scan\\2023-11-20-14-02_57570d8\\scan\\output_0_b348476b.pb.zst")
+	r, _ := NewFileReader("testdata/example.com.commit=d8c45a9.pb.zst")
 	r.Zip = &zip
 
-	fmt.Sprintf("ttttt")
 	out := make(chan resolver.Result)
 	go func() {
 		if err := r.ReadTo(out); err != nil {
@@ -21,6 +21,37 @@ func TestReader_ReadTo(t *testing.T) {
 	}()
 
 	for result := range out {
-		fmt.Println(result.Domains)
+		if len(result.Domains) == 0 {
+			t.Errorf("Expected the file to contain 'example.org.', found empty array")
+		}
+
+		if !result.Domains[0].Name.EqualString("example.com.") {
+			t.Errorf("Expected the file to contain 'example.org.', found %v", result.Domains[0].Name)
+		}
+	}
+}
+
+// TestReader_CanReadLegacyFileFormat tests if the reader can read the file format of the published data
+// that uses 4 byte short UUIDs and 16 byte long UUIDs (when the 4 byte UUIDs is not unique.)
+func TestReader_CanReadPublishedFileFormat(t *testing.T) {
+	zip := serialization.ZipZSTD
+	r, _ := NewFileReader("testdata/example.com.commit=d8c45a9.pb.zst")
+	r.Zip = &zip
+
+	out := make(chan resolver.Result)
+	go func() {
+		if err := r.ReadTo(out); err != nil {
+			panic(err)
+		}
+	}()
+
+	for result := range out {
+		if len(result.Domains) == 0 {
+			t.Errorf("Expected the file to contain 'example.org.', found empty array")
+		}
+
+		if !result.Domains[0].Name.EqualString("example.com.") {
+			t.Errorf("Expected the file to contain 'example.org.', found %v", result.Domains[0].Name)
+		}
 	}
 }
